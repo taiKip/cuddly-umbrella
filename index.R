02e1d2a0abda60f924afcd8bebd79bf9d7e98cf2
tarus <victortarus6@gmail.com>
2026-03-24 23:37:43 +1030

    fixed error in penguin plot

--- index.R
+++ index.R
@@ -1,20 +1,4 @@
-library('ggplot2')
-#check working directory
-getwd()
+library(ggplot2)
+library(palmerpenguins)
 
-#import your .csv file to your global environment
-
-wforecasts <- read.csv("data/weather_forecasts.csv",header = TRUE,sep =",")
-
-head(df)
-
-
-#Base R
-plot(wforecasts$forecast_temp,wforecasts$observed_temp)
-
-
-#ggplot
-
-ggplot(wforecasts,aes(x=forecast_temp,y=observed_temp))+geom_point()
-
-
+ggplot(penguins) + geom_point(aes (x= bill_length_mm,y= bill_depth_mm, colour = species))
